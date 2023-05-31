707e43af1ba73902bb92c0d8643458b07b677105
Fabio Oliveira <fabiosousaoliveira15@gmail.com>
2023-05-31 12:00:29 +0200

    Added DIM project and removed Clock

--- index.js
+++ index.js
@@ -30,20 +30,3 @@ logoDesc.addEventListener('animationiteration', () => {
 
     logoDesc.style.setProperty('--customHue', availableColors[randomColor].toString());
 });
-
-/* The clock on the top right of the screen */
-function currentTime() {
-    let date = new Date();
-    let hh = date.getHours();
-    let mm = date.getMinutes();
-    let ss = date.getSeconds();
-
-    hh = (hh < 10) ? "0" + hh : hh;
-    mm = (mm < 10) ? "0" + mm : mm;
-    ss = (ss < 10) ? "0" + ss : ss;
-
-    document.getElementById("clock").innerText = hh + ":" + mm + ":" + ss;
-    setTimeout(function(){ currentTime() }, 1000);
-}
-
-currentTime();
