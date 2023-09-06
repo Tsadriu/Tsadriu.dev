e60776d60ca9be6d111bc68a12c6306875bf0387
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-09-06 15:27:24 +0200

    Update code format and improve logo color transition
    Code on various web pages was reformatted for easier readability, this involved adding line breaks and indentation to longer sections. Additionally, the color transition on the '.dev' text part of the website logo was improved. Instead of a blinking effect, colors will now increment in hue gradually, creating a smoother transition. This achieves a more desirable aesthetic for the logo and enhances user experience. Regular intervals were also set for the incrementation.

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-window.addEventListener('scroll', (event) => {
-    let scrollPosition = window.scrollY;
-    let headerElement = document.querySelector('#header');
+window.onload = function () {
+    const setAge = () => {
+        const birthDate = new Date('2000-03-25');
+        const todayDate = new Date();
+        let age = todayDate.getFullYear() - birthDate.getFullYear();
+        const month = todayDate.getMonth() - birthDate.getMonth();
+        const day = todayDate.getDate() - birthDate.getDate();
 
-    if(scrollPosition > headerElement.offsetHeight){
-        headerElement.style.background = "black";
-    } else {
-        headerElement.style.background = "linear-gradient(to top right, rgba(0,0,0,0.3) 30%, rgba(0,0,0,1) 90%), url('assets/images/bg.png')";
+        if (month < 0 || (month === 0 && todayDate.getDate() < birthDate.getDate())) {
+            age--;
+        }
+
+        document.getElementById('age').textContent = age;
     }
-});
+
+    setAge();
+}
+
+// Get the button:
+let goTopButton = document.getElementById("goTopButton");
+
+window.onscroll = function() {
+    checkForPosition()
+};
+
+function checkForPosition() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+    {
+        goTopButton.style.display = "block";
+    }
+    else
+    {
+        goTopButton.style.display = "none";
+    }
+}
+
+function goTopPage() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+/* Animation of the .dev text */
+const logoDesc = document.querySelector('.logo-desc i');
+
+// Initialize hue
+logoDesc.style.setProperty('--customHue', '0');
+
+// Interval to increment hue
+setInterval(() => {
+    let currentHue = parseInt(logoDesc.style.getPropertyValue('--customHue'));
+    let newHue = (currentHue + 10) % 360; // change hue value gradually.
+
+    logoDesc.style.setProperty('--customHue', newHue);
+}, 475);
