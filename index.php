beb71ba46e512a6195b3af608075e66d703dbfa5
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-10-09 23:02:56 +0200

    Add skill images and a PHP class for fetching skill-related data
    The "index.html" file is deleted since it's replaced by the "index.php". This allows us to dynamically generate the skill section of the webpage using data returned by 'SkillsController'. In addition, several skill images have been added, and several minor modifications have been done to other page components, e.g., change the return link in "Arcadia". As for the logic of skill presentation, it' handled by the new PHP class 'SkillsController' through its method 'getSkills' which returns an array of skill-related information.

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="assets/images/logo_round.png">
+    <title>
+        [Tsadriu] Fabio Oliveira
+    </title>
+</head>
+<body>
+<section class="header">
+    <img src="assets/images/logo.png" alt="Logo icon" class="logo">
+    <div>
+        <h1>FABIO OLIVEIRA DE SOUSA</h1>
+        <h2>Apprentice Software Developer</h2>
+    </div>
+    <div class="social-icons">
+        <a href="https://discord.com/users/403650027169513472" target="_blank">
+            <i class="fa-brands fa-discord"></i>
+        </a>
+        <a href="https://github.com/Tsadriu" target="_blank">
+            <i class="fa-brands fa-github"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/fabio-oliveira-de-sousa-2328a21b5/" target="_blank">
+            <i class="fa-brands fa-linkedin-in"></i>
+        </a>
+    </div>
+</section>
+<div id="body">
+    <h2>My Skills</h2>
+    <hr>
+    <?php include './views/skills.php'; ?>
+    <h2>My Projects</h2>
+    <hr>
+    <?php include './views/projects.php'; ?>
+</div>
+</body>
+</html>
---
+++ views/projects.php
@@ -0,0 +1,28 @@
+<?php
+include './controllers/ProjectsController.php';
+
+$controller = new ProjectsController();
+$projects = $controller->getProjects();
+?>
+<div class="my-projects-section">
+    <?php foreach ($projects as $project): ?>
+        <div class="project-bubble">
+            <div>
+                <a href="<?php echo $project['link'] ?>" target="_blank">
+                    <img class="project-bubble-img" src="<?php echo $project['image']; ?>"
+                         alt="<?php echo $project['alt']; ?>">
+                </a>
+            </div>
+            <div class="project-name">
+                <?php echo $project['name']; ?>
+            </div>
+            <div class="project-description">
+                <?php echo $project['description']; ?>
+            </div>
+            Made-in:
+            <div class="project-made-in">
+                <?php echo implode(", ", $project['made-in']); ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+</div>
